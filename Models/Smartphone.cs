c5912a3e3e5e5e12e4ba8a5d5d748bc3239df88a
Matheus Magari <matheus_magari@outlook.com>
2023-01-29 23:34:06 -0300

    Conclusão do desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            this.Numero = numero;
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +23,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public string DadosTelefone()
+        {
+            return $"Número: {this.Numero}, Modelo: {this.Modelo}, IMEI: {this.IMEI}, Memória: {this.Memoria}";
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
